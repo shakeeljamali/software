eeae21849102925861ad6a77321d4cc3d44f873a
unknown <Shakeeljamali@gmail.com>
2018-05-16 19:37:43 +0500

    second

--- Example4.java
+++ Example4.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 class Example4{
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
@@ -26,3 +26,36 @@ class Example4{
 		System.out.println("grade="+grade);
 		
 	}}
+	*/
+	
+	
+	
+	
+	//// shakeel jamali baloch
+	public class Example5 {
+
+   public static void main(String args[]) {
+      // char grade = args[0].charAt(0);
+      char grade = 'C';
+
+      switch(grade) {
+         case 'A' :
+            System.out.println("Excellent!"); 
+            break;
+         case 'B' :
+
+            System.out.println("Well done");
+            break;
+         case 'D' :
+            System.out.println("You passed");
+         case 'F' :
+            System.out.println("Better try again");
+            break;
+         default :
+            System.out.println("Invalid grade");
+      }
+      System.out.println("Your grade is " + grade);
+   }
+}
+
+
